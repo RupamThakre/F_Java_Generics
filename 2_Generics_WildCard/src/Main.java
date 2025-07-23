815c7945210dc60a90028cb1688284edf03909ba
RupamThakre <rupamthakre15@gmail.com>
2025-07-23 23:09:34 +0530

    WildCard in Generics

---
+++ 2_Generics_WildCard/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+/*
+* ? - represent unknown type
+*
+* 3 Main use cases
+*
+* 1. Unknown types - List<?> list
+* 2. Upper bound - List<? extends Number>
+* 3. Lower bound - List<? super Integer>
+* */
+public class Main {
+    public static void main(String[] args) {
+        //UseCase-01 : Unknown Types
+        public void printList(List<?> list) {
+            for (Object elem : list) {
+                System.out.println(elem);
+            }
+        }
+
+        //UseCase-02: Upper Bounded Wildcard: <? extends T>
+        public void process(List<? extends Number> list) {
+            for (Number n : list) {
+                System.out.println(n.doubleValue());
+            }
+        }
+
+        //UseCase03: Lower Bounded Wildcard: <? super T>
+        public void addNumbers(List<? super Integer> list) {
+            list.add(42);  // OK
+        }
+    }
+}
